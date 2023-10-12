42fc405595fcf1bf0151ff63aeedefe618a8ba8b
qcrao <qcrao91@gmail.com>
2023-10-12 18:12:19 +0800

    add delay after click delete button

--- bulkDeleteConversations.js
+++ bulkDeleteConversations.js
@@ -11,7 +11,6 @@ async function bulkDeleteConversations() {
 
   // 使用 for 循环按顺序删除选中的对话
   for (const element of selectedConversations) {
-    await delay(300);
     await deleteConversation(element);
   }
 }
@@ -28,7 +27,7 @@ function removeAllCheckboxes() {
 async function deleteConversation(checkbox) {
   const conversationElement = await checkbox.closest('.flex.p-3.items-center.gap-3.relative.rounded-md')
 
-  console.log("1. 开始删除", conversationElement);
+  console.log("1. 点击对话", conversationElement);
   conversationElement.click(); // 点击对话
 
   // debugger
@@ -36,10 +35,10 @@ async function deleteConversation(checkbox) {
   const deleteButton = await waitForElement("nav div a>div>button:nth-child(2)");
 
   if (deleteButton) {
-      await delay(100);
       console.log("2. 点击删除按钮");
 
       deleteButton.click(); // 点击删除按钮
+      await delay(300);
 
       // 等待确认删除对话框的按钮出现
       const confirmButton = await waitForElement('button.btn.btn-danger');
